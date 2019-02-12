1f3db7f32d95a62b92b5ade11f6cf2ff23ab1bec
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2019-02-12 10:43:00 +0100

    quick setup interface for action and reducer function

--- app-1.ts
+++ app-1.ts
@@ -1,9 +1,24 @@
 // Actions
 interface Action {
-    type: string;
-    payload?: any;
+  type: string;
+  payload?: any;
 }
 
-const incrementAction: Action = {
-    type: 'Increment'
+/* const incrementAction: Action = {
+  type: 'Increment'
+} */
+
+function reducer(state = 10, action: Action) {
+  if ( action.type === 'Increment') {
+    return state += 1;
+  }
+
+  return state
 }
+
+// Reducer
+const incrementAction: Action = {
+  type: 'Increment'
+}
+
+console.log( reducer(10, incrementAction ));

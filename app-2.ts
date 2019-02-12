683d64237751b62b97536d0633f1cd901e8a4229
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2019-02-12 11:57:30 +0100

    organizing files, separating actions file

---
+++ app-2.ts
@@ -0,0 +1,37 @@
+import { Action } from './ngrx-fake/ngrx';
+import { 
+    incrementAction, 
+    decrementAction, 
+    multiplyAction, 
+    divideAction, 
+    resetAction } from './counter/counter.actions';
+ 
+function reducer(state = 10, action: Action) {
+    
+      switch( action.type ) {
+    
+        case 'INCREMENT':
+          return state += 1;
+    
+        case 'DECREMENT':
+          return state -= 1;
+    
+        case 'MULTIPLY':
+          return state * action.payload;
+    
+          case 'DIVIDE':
+          return state / action.payload;
+
+          case 'RESET':
+          return state = 0;
+    
+        default:
+          return state;
+      }
+    }
+
+console.log( reducer(10, incrementAction)); // 11
+console.log( reducer(10, decrementAction)); // 9
+console.log( reducer(10, multiplyAction)); // 20
+console.log( reducer(10, divideAction)); // 5
+console.log( reducer(10, resetAction)); // 0

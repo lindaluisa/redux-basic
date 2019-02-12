1e49bb34fe3ee6ad96abb7144c0d44a6067fa744
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2019-02-12 11:19:23 +0100

    simple switch function to familiarize with reducer fn and action interface

--- app-1.ts
+++ app-1.ts
@@ -4,21 +4,54 @@ interface Action {
   payload?: any;
 }
 
-/* const incrementAction: Action = {
-  type: 'Increment'
-} */
 
 function reducer(state = 10, action: Action) {
-  if ( action.type === 'Increment') {
+/*   if ( action.type === 'Increment') {
     return state += 1;
-  }
+  } */
 
-  return state
+  switch( action.type ) {
+
+    case 'INCREMENT':
+      return state += 1;
+
+    case 'DECREMENT':
+      return state -= 1;
+
+    case 'MULTIPLY':
+      return state * action.payload;
+
+      case 'DIVIDE':
+      return state / action.payload;
+
+    default:
+      return state;
+  }
 }
 
 // Reducer
 const incrementAction: Action = {
-  type: 'Increment'
+  type: 'INCREMENT'
 }
 
-console.log( reducer(10, incrementAction ));
+console.log( reducer(10, incrementAction) ); // 11
+
+const decrementAction: Action = {
+  type: 'DECREMENT'
+}
+
+console.log( reducer(10, decrementAction) ); // 9
+
+const multiplyAction: Action = {
+  type: 'MULTIPLY',
+  payload: 2
+}
+
+console.log( reducer(10, multiplyAction) ); //20
+
+const divideAction: Action = {
+  type: 'DIVIDE',
+  payload: 2
+}
+
+console.log( reducer(10, divideAction) ); // 5
